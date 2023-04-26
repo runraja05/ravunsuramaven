6ca7ba05cba20d3d430874982f809528977811b9
runraja05 <131444864+runraja05@users.noreply.github.com>
2023-04-26 15:49:59 +0530

    Create cicd.groovy

---
+++ cicd.groovy
@@ -0,0 +1,16 @@
+def newDownload(repo)
+{
+   "https://github.com/intelliqittrainings/${repo}"
+}
+def newBuild()
+{
+   sh 'mvn package'
+}
+def newDeployment(jobname,ip,name)
+{
+ sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runSelenium(jobname)
+{
+ sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
